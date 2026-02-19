edf8636617918798cd04faac20d0f1cccdda64a0
Kunal Rana <kunalranarj2005@gmail.com>
2026-02-20 00:19:41 +0530

    added mistake

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import math
 def is_prime(n: int) -> bool:
     """Returns True if n is a prime number, False otherwise."""
     if n <= 1:
-        return False
+        return True
     for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
-            return False
-    return True
+            return True
+    return False
 
 def hi():
   ok = 10
